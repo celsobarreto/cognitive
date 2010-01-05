0b1f03eff45f38d9182ac7f3078551e25dbf5251
adam.dziendziel <adam.dziendziel@ffa0edf4-cbd0-11de-bef2-819d840586c7>
2010-01-05 11:45:05 +0000

    fixes for sending email

--- cognitive/src/hot/edu/uj/cognitive/action/EmailSender.java
+++ cognitive/src/hot/edu/uj/cognitive/action/EmailSender.java
@@ -6,5 +6,11 @@ import javax.mail.MessagingException;
 @Local
 public interface EmailSender {
 	public void postMail( ) throws MessagingException;
-
+	public void setRecipient(String v);
+	public void setSubject(String v);
+	public void setMessage(String v);
+	public String getRecipient();
+	public String getSubject();
+	public String getMessage();
+	public void destroy();
 }
