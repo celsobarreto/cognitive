dc663db7b7f22a607c28b460b6f25e60b5367c78
mateusz.kosibowicz <mateusz.kosibowicz@ffa0edf4-cbd0-11de-bef2-819d840586c7>
2010-01-07 23:30:50 +0000

    wyswietlanie podgladu strony statycznej po wybraniu z listy

--- trunk/cognitive/src/hot/edu/uj/cognitive/action/PageEdit.java
+++ trunk/cognitive/src/hot/edu/uj/cognitive/action/PageEdit.java
@@ -5,8 +5,8 @@ import javax.ejb.Local;
 @Local
 public interface PageEdit
 {
-	public void showPage(String page);
-	public void savePage(String page);
+	public void showPage();
+	public void savePage();
 	public void showList();
 	public void destroy();
 }
