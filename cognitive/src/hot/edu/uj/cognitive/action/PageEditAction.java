8e303174541ffaa894ce45da2ebb6dca02e9302b
mateusz.kosibowicz <mateusz.kosibowicz@ffa0edf4-cbd0-11de-bef2-819d840586c7>
2010-01-07 23:30:50 +0000

    wyswietlanie podgladu strony statycznej po wybraniu z listy

--- cognitive/src/hot/edu/uj/cognitive/action/PageEditAction.java
+++ cognitive/src/hot/edu/uj/cognitive/action/PageEditAction.java
@@ -6,16 +6,17 @@ import javax.ejb.Remove;
 import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import org.jboss.seam.ScopeType;
+import javax.persistence.Query;
+
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Out;
 import org.jboss.seam.annotations.datamodel.DataModel;
 import org.jboss.seam.annotations.datamodel.DataModelSelection;
+import org.jboss.seam.annotations.web.RequestParameter;
 import org.jboss.seam.log.Log;
 
 import edu.uj.cognitive.model.SpecialPage;
-import edu.uj.cognitive.model.User;
 
 @Stateful
 @Name("pageEdit")
@@ -38,16 +39,23 @@ public class PageEditAction implements PageEdit
 	@PersistenceContext
 	private EntityManager em;
 	
-	public void showPage(String page)
+	@RequestParameter
+	private String pageId;
+	
+	@SuppressWarnings("unchecked")
+	public void showPage()
 	{
-		log.info("authenticating {0}", page);
-		this.sPage = this.em.find(SpecialPage.class, page);
-		this.pageID = (this.sPage == null ? page : this.sPage.getId());
-	}
-	public void savePage(String page){
+		System.out.println(this.pageId); 
 		
+		log.info(this.pageId);
+		Query query = this.em.createQuery("select u from SpecialPage u where u.id = :pageId");
+		query.setParameter("pageId", this.pageId);
+		this.sPage = (SpecialPage) query.getSingleResult();
+		this.pageID = (this.sPage == null ? "error": this.sPage.getId());
 	}
+
 	public void showList(){
+		log.info("showList");
 		this.pageList = this.em.createQuery("select u from SpecialPage u").getResultList();
 	}
 	
@@ -58,5 +66,10 @@ public class PageEditAction implements PageEdit
 			this.sPage = null;
 		if (this.pageID != null)
 			this.pageID = "";
+	}
+	@Override
+	public void savePage() {
+		// TODO Auto-generated method stub
+		
 	}
 }
