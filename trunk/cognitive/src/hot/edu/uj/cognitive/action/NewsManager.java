38d85fe9b63ee6dc151786e4b7e3ac149b948306
pawcyk86 <pawcyk86@ffa0edf4-cbd0-11de-bef2-819d840586c7>
2010-01-15 23:07:28 +0000

    dodwanie, edycja i usuwanie wiadomosci

--- trunk/cognitive/src/hot/edu/uj/cognitive/action/NewsManager.java
+++ trunk/cognitive/src/hot/edu/uj/cognitive/action/NewsManager.java
@@ -2,11 +2,17 @@ package edu.uj.cognitive.action;
 
 import javax.ejb.Local;
 
+import edu.uj.cognitive.model.News;
+
 @Local
 public interface NewsManager 
 {
 	public void destroy();
-	public void getNews();
-	public void addNews();
-	public void createNews();
+	public void getListOfNews();
+	public String addNewsAction();
+	public String editNewsAction();
+	public String deleteNewsAction();
+	public String performNewsAction();
+	public News getNews();
+	public void setNews(News news);
 }
