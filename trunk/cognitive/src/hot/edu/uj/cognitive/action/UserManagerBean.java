1f8c1e395198f80ea0e4f359eb2dbe6a82cb702f
piotr.ladanowski <piotr.ladanowski@ffa0edf4-cbd0-11de-bef2-819d840586c7>
2010-01-23 16:25:24 +0000

    wyszukiwarki - przedsiebiorcy/naukowcy

--- trunk/cognitive/src/hot/edu/uj/cognitive/action/UserManagerBean.java
+++ trunk/cognitive/src/hot/edu/uj/cognitive/action/UserManagerBean.java
@@ -1,34 +1,46 @@
 package edu.uj.cognitive.action;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import javax.ejb.Remove;
 import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
+
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Factory;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Out;
+import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.datamodel.DataModel;
-import org.jboss.seam.annotations.datamodel.DataModelSelection;
 import org.jboss.seam.annotations.web.RequestParameter;
 
-import edu.uj.cognitive.model.Publication;
-import edu.uj.cognitive.model.SpecialPage;
+import edu.uj.cognitive.model.Role;
+import edu.uj.cognitive.model.ScienceDomain;
 import edu.uj.cognitive.model.User;
 
 @Stateful
 @Name("userManager")
+@Scope(ScopeType.SESSION)
 public class UserManagerBean implements UserManager
 {
 	@PersistenceContext(type = PersistenceContextType.EXTENDED)
 	private EntityManager em;
 	
-	@DataModel
+	@DataModel(scope = ScopeType.PAGE)
 	private List<User> userList;
 	
 	@RequestParameter 
 	private Integer userId;
+	
+	private String mode;
+	
+	private String searchText;
+	
+	private String searchCriteria;
 	
 	public Integer getUserId() {
 		return userId;
@@ -45,9 +57,79 @@ public class UserManagerBean implements UserManager
 	@Factory("userList")
 	public void list()
 	{
-		this.userList = this.em.createQuery("select u from User u").getResultList();
+		if(searchCriteria != null && !searchCriteria.equals("scienceDomains")){
+			String search = "%"+searchText.replace("*", "%")+"%";
+			this.userList = this.em.createQuery("select u from User u where u."+searchCriteria+
+					" like '"+search+"' order by u.fullName").getResultList();
+		}else{
+			this.userList = this.em.createQuery("select u from User u").getResultList();
+		}
+		selectUsers();
+		if(searchCriteria != null && searchCriteria.equals("scienceDomains")){
+			searchUserScienceDomains();
+		}
+		searchText = null;
+		searchCriteria = null;
+	}
+	
+	private void searchUserScienceDomains(){
+		Set<User> tmpList = new HashSet<User>();
+		for(User u : userList){
+			if(u.getScienceDomains() != null && u.getScienceDomains().size()>0){
+				for(ScienceDomain s : u.getScienceDomains()){
+					if(!s.getName().contains(searchText)){
+						tmpList.add(u);
+					}
+				}
+			}else{
+				tmpList.add(u);
+			}
+		}
+		userList.removeAll(tmpList);
+	}
+	
+	private void selectUsers(){
+		Set<User> tmpList = new HashSet<User>();
+		for(User u : userList){
+			if(u.getRoles() != null && u.getRoles().size()>0){
+				for(Role r : u.getRoles()){
+					System.out.println(mode);
+					if(!r.getName().equals(mode)){
+						tmpList.add(u);
+					}
+				}
+			}else{
+				tmpList.add(u);
+			}
+		}
+		userList.removeAll(tmpList);
 	}
 
+	public void setSearchText(String text){
+		this.searchText = text;
+	}
+	
+	public String getSearchText(){
+		return searchText;
+	}
+	
+	public String getSearchCriteria() {
+		return searchCriteria;
+	}
+
+	public void setSearchCriteria(String searchCriteria) {
+		this.searchCriteria = searchCriteria;
+	}
+
+	public String getMode() {
+		return mode;
+	}
+
+	public void setMode(String mode) {
+		this.destroy();
+		this.mode = mode;
+	}
+	
 	public void delete() 
 	{
 		if (this.user != null)
@@ -65,8 +147,7 @@ public class UserManagerBean implements UserManager
 		this.userList = null;
 		this.user = null;
 	}
-	
-	@SuppressWarnings("unchecked")
+
 	public void show()
 	{
 		
